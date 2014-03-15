ab7b436f0dd9f6a3c97f819c00efde700dc263f9
Sebastian Ortega <sortega@tid.es>
2014-03-15 19:33:27 +0100

    red

--- src/test/java/redpencil/ProductTest.java
+++ src/test/java/redpencil/ProductTest.java
@@ -11,4 +11,12 @@ public class ProductTest {
         Product instance = new Product(new Currency(100));
         assertEquals(new Currency(100), instance.currentPrice());
     }
+
+    @Test
+    public void shouldRegisterPriceChanges() throws Exception {
+        Product instance = new Product(new Currency(100));
+        instance.changePrice(new Currency(99, 99));
+        instance.changePrice(new Currency(80));
+        assertEquals(new Currency(80), instance.currentPrice());
+    }
 }
