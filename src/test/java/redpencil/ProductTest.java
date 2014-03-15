ba867f5653441764cc694a4d3d4dd8aa12298375
Sebastian Ortega <sortega@tid.es>
2014-03-15 19:45:37 +0100

    yellow

--- src/test/java/redpencil/ProductTest.java
+++ src/test/java/redpencil/ProductTest.java
@@ -1,20 +1,26 @@
 package redpencil;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ProductTest {
 
+    private Product instance;
+
+    @Before
+    public void setUp() throws Exception {
+        instance = new Product(new Currency(100));
+    }
+
     @Test
     public void shouldHaveACurrentPrice() throws Exception {
-        Product instance = new Product(new Currency(100));
         assertEquals(new Currency(100), instance.currentPrice());
     }
 
     @Test
     public void shouldRegisterPriceChanges() throws Exception {
-        Product instance = new Product(new Currency(100));
         instance.changePrice(new Currency(99, 99));
         instance.changePrice(new Currency(80));
         assertEquals(new Currency(80), instance.currentPrice());
