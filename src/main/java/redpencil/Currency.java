ed5468030ef24a1a31ca52050aa0efe247fdb80f
Sebastian Ortega <sortega@tid.es>
2014-03-15 19:31:07 +0100

    green

--- src/main/java/redpencil/Currency.java
+++ src/main/java/redpencil/Currency.java
@@ -1,8 +1,36 @@
 package redpencil;
 
-public class Currency {
+import java.text.NumberFormat;
+
+public final class Currency {
+
+    private final int decimals;
+    private final int amount;
 
     public Currency(int amount) {
+        this.decimals = localCurrencyFormat().getMaximumFractionDigits();
+        this.amount = amount * decimals;
+    }
 
+    @Override
+    public String toString() {
+        return localCurrencyFormat().format((double) amount / decimals);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency currency = (Currency) o;
+        return amount == currency.amount && decimals == currency.decimals;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * decimals + amount;
+    }
+
+    private static NumberFormat localCurrencyFormat() {
+        return NumberFormat.getCurrencyInstance();
     }
 }
--- src/main/java/redpencil/Product.java
+++ src/main/java/redpencil/Product.java
@@ -2,6 +2,13 @@ package redpencil;
 
 public class Product {
 
+    private final Currency price;
+
     public Product(Currency initialPrice) {
+        this.price = initialPrice;
+    }
+
+    public Currency currentPrice() {
+        return price;
     }
 }
